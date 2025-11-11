114fb4b6c21c51afd7552d1e3de5ceb6e623e3ac
rporter03 <porterclayton2003@gmail.com>
2025-11-10 21:32:35 -0500

    Server Side for Niche Sounds

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./public/images/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+
+  let artists = [
+    {
+        "_id": 1,
+        "artist": "Forth Wanderers",
+        "genre": "Indie Rock",
+        "Description": "Forth Wanderers are an Indie Rock band with a unique sound. The use unique melodies that are hard to predict where they go next, which makes an enjoyable listening experience. They have released multiple albums and EPs since 2013, with their most recent album releasing earlier this year in July 2025.",
+        "img": "images/forth-wanderers.jpg"
+    },{
+        "_id": 2,
+        "artist": "The Strokes",
+        "genre": "Alternative/Indie",
+        "Description": "The Strokes is a band that needs no mention; they are one of the most popular alternative/indie bands out there, with hundreds of millions of plays for multiple songs. They have a wide discography and most recently have put out an album in 2020 and 2023.",
+        "img": "images/the-strokes.jpg"
+    },{
+        "_id": 3,
+        "artist": "Battles",
+        "genre": "Experimental Rock",
+        "Description": "Battles is a band that produces experimental rock music. They have a very unique sound as their music is composed of unnatural rhythms and intervals, but create an extraordinary sound. They have been active since the mid 2000s and have most recently released multiple EP's in 2020.",
+        "img": "images/battles.jpg"
+    },{
+        "_id": 4,
+        "artist": "Should",
+        "genre": "Alternative/Indie",
+        "Description": "Should is an alternative/indie band that has been occasionally active since the late 90s. As of 2025, they have released two singles after a long eleven year gap. They make great indie music with hints of shoegaze.",
+        "img": "images/should.jpg"
+    },{
+        "_id": 5,
+        "artist": "Kind of Like Spitting",
+        "genre": "Indie Rock",
+        "Description": "Kind of Like Spitting was an indie rock band that were active in the late 90s, early 2000s, and recently again in 2025. The band has a wide discography and have just released their final album this October, marking the end of the band.",
+        "img": "images/spitting.jpg"
+    },{
+        "_id": 6,
+        "artist": "Tabar",
+        "genre": "Math Pop",
+        "Description": "Tabar was a math pop band that was very short lived, only releasing one EP with three songs in 2010. However, their sound is incredible and very enjoyable to listen to. Their recent whereabouts are unknown and recent forum posts suggest they have no presence online.",
+        "img": "images/tabar.png"
+    },{
+        "_id": 7,
+        "artist": "Adult Mom",
+        "genre": "Alternative/Indie",
+        "Description": "Adult Mom is the project of musician Stevie Knipe, who was originally the only member. However, Adult Mom has grown to become a full band with multiple members. Adult Mom has released six full length albums with a multitude of singles. Their sound can be described as soft, sad, and very emotional.",
+        "img": "images/adult-mom.jpg"
+    },{
+        "_id": 8,
+        "artist": "Brittle Stars",
+        "genre": "Indie Pop",
+        "Description": "Coming from Florida in the late 90s, Brittle Stars were an indie pop band with short, sweat, and dreamy sounding songs. They have released two albums and as of more recently, have released two singles as well. The band was composed of four members who each brought their own instrument to life in their sounds.",
+        "img": "images/brittle-stars.webp"
+    }
+]
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/artists/", (req, res)=>{
+    console.log("in get request")
+    res.send(artists);
+});
+
+app.get("/api/artists/:id", (req, res)=>{
+    const artist = artists.find((artist)=>artist._id === parseInt(req.params.id));
+    res.send(artist);
+});
+
+
+app.listen(3001, () => {
+    console.log("Server is up and running");
+});
