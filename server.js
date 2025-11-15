8991a0525db790f09c960769588d661a4e3cfa18
rporter03 <porterclayton2003@gmail.com>
2025-11-15 16:16:13 -0500

    Testing to get images to work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(cors());
 
